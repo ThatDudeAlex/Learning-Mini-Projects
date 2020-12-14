9df0ca978817806af09f0e155066424658622c60
Jose A Nunez <josenunez1692@gmail.com>
2020-12-13 21:05:20 -0500

    experimented reading/writing to files using Path & Files Objects

---
+++ Java/Input & Output With Streams & Files/UsingFilesAndTypes.java
@@ -0,0 +1,70 @@
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
+
+// Small program to practice working with files, using Path & Files types
+public class UsingFilesAndTypes {
+
+    // Constructor 
+    UsingFilesAndTypes(){}
+
+    public static void main(String[] args) throws Exception {
+        UsingFilesAndTypes reader = new UsingFilesAndTypes();
+
+        // get object representation of file paths 
+        Path readPath = Paths.get("readerTest.txt");
+        Path writePath = Paths.get("writerTest.txt");
+
+        reader.readFromFile(readPath);
+        reader.writeToFile(writePath);
+        reader.appendToFileAndReadIt(readPath, writePath);
+        reader.deleteOutputFile(writePath);
+    }
+
+
+    // Reads all the lines in readerTest.txt and prints them to the console
+    public void readFromFile(Path path) throws Exception{
+        List<String> lines = Files.readAllLines(path);
+
+        for(String line : lines) 
+            System.out.println(line);
+
+    }
+
+    // Writes the data array into writerTest.txt 
+    public void writeToFile(Path path) throws Exception {
+        String[] data = {
+            "Starting Writer Test",
+            "Writing 1",
+            "Writing 1 2",
+            "Writing 1 2 3",
+            "Writing Testing Complete"
+        };
+
+        Files.write(path, Arrays.asList(data));
+        System.out.println("\nFinished Writing To File");
+    }
+
+    // Appends the contents of the readerTest.txt file into the writerTest.txt file,
+    // then opens & reads writerTest.txt to print it to the console
+    public void appendToFileAndReadIt(Path readPath, Path writePath) throws Exception{
+        System.out.println("\n--- Preparing Read/Write Pipeline ---\n");
+
+        List<String> data = new ArrayList<>();
+        data.add("\n -- Appended readInput --\n");
+        data.addAll(Files.readAllLines(readPath));
+
+        Files.write(writePath, data, StandardOpenOption.APPEND);
+        readFromFile(writePath);
+
+        System.out.println("\n--- Finished Read/Write Pipeline ---");
+    }
+
+    // Clean up by deleting writerTest.txt
+    public void deleteOutputFile(Path path) throws Exception{
+        Files.delete(path);
+        System.out.printf("\nPath: %s was just deleted", path.toString());
+    }
+}
